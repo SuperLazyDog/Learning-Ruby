a06cda33cad17a7c68f1b445cc3e98682844b04e
SuperLazyDog <jyoitatsu@foxmail.com>
2017-05-07 12:46:08 +0900

    P24

--- Test/HelloRuby.rb
+++ Test/HelloRuby.rb
@@ -1,1 +1,16 @@
 print("Hello, Ruby.\n")
+puts("Hello, Ruby.")
+p("Hello Ruby.\n")
+print(1+1, "\n")
+print(2+2, "\n")
+#20
+intX = 10
+intY = 20
+intZ = 30
+volume = intX*intY*intZ
+area = (intX*intY + intY*intZ + intZ+intX) * 2
+print("面積：#{area}\n", "体積： #{volume}\n")
+#24
+print(intX == intY, "\n")
+print(intX > intY, "\n")
+print(intX < intY, "\n")
