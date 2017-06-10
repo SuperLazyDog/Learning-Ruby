a2894acce51d7d1dbcded698e6e84a2ea01c0f44
SuperLazyDog <jyoitatsu@foxmail.com>
2017-06-10 11:19:44 +0900

    add some confirm test

---
+++ Test/Pro/check.rb
@@ -0,0 +1,24 @@
+puts("----------------------------------------")
+puts("                  1.1")
+puts("----------------------------------------")
+asa = nil
+asa = [1,2]
+puts("asa#{asa}")
+print("defined?: ", defined? asa);puts()
+print("nil?: ", asa.nil?);puts()
+print("empty?: ", asa.empty?);puts()
+#print("blank?: ");print(asa.blank?);puts()
+#print("presence?: ");print(asa.presence?);puts()
+puts("----------------------------------------")
+puts("                  1.2")
+puts("----------------------------------------")
+local = 1
+Const = 1
+$global = 1
+@instance = 1
+@@class = 1
+print("local: ", defined?(local));puts()
+print("Const: ", defined?(Const));puts()
+print("$global: ", defined?($global));puts()
+print("@instance: ", defined?(@instance));puts()
+print("@@class: ", defined?(@@class));puts()
