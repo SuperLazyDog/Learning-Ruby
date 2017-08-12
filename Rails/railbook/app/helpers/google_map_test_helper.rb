42b8b2eb030cc935198f62ed43c1d918f2ffb891
SuperLazyDog <jyoitatsu@foxmail.com>
2017-08-13 08:03:41 +0900

    Google Maps Apiについての勉強の準備

---
+++ Rails/railbook/app/helpers/google_map_test_helper.rb
@@ -0,0 +1,2 @@
+module GoogleMapTestHelper
+end
---
+++ Rails/railbook/test/controllers/google_map_test_controller_test.rb
@@ -0,0 +1,54 @@
+require 'test_helper'
+
+class GoogleMapTestControllerTest < ActionDispatch::IntegrationTest
+  test "should get t1" do
+    get google_map_test_t1_url
+    assert_response :success
+  end
+
+  test "should get t2" do
+    get google_map_test_t2_url
+    assert_response :success
+  end
+
+  test "should get t3" do
+    get google_map_test_t3_url
+    assert_response :success
+  end
+
+  test "should get t4" do
+    get google_map_test_t4_url
+    assert_response :success
+  end
+
+  test "should get t5" do
+    get google_map_test_t5_url
+    assert_response :success
+  end
+
+  test "should get t6" do
+    get google_map_test_t6_url
+    assert_response :success
+  end
+
+  test "should get t7" do
+    get google_map_test_t7_url
+    assert_response :success
+  end
+
+  test "should get t8" do
+    get google_map_test_t8_url
+    assert_response :success
+  end
+
+  test "should get t9" do
+    get google_map_test_t9_url
+    assert_response :success
+  end
+
+  test "should get t10" do
+    get google_map_test_t10_url
+    assert_response :success
+  end
+
+end
