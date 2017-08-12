1a1643af6d131402c27a596e33ad19b3dc8b38a3
SuperLazyDog <jyoitatsu@foxmail.com>
2017-08-12 16:34:24 +0900

    scrapping学习与练习

--- Test/Pro/scraping.rb
+++ Test/Pro/scraping.rb
@@ -92,3 +92,71 @@ store_access_map_link = store_page.at('/html/body/div[1]/div[4]/div/div/p[2]/a')
 store_access_map_page = agent.get(store_access_map_link)
 store_access_map = store_access_map_page.at('#mapbox')
 puts store_access_map
+
+
+#------------------------------------------------------------------
+=begin                      店舗情報の例
+#------------------------------------------------------------------
+店名：（株）川内自動車　ビックカードーム　タンポポ
+写真：ccsrpcma.carsensor.net/shopinfo/images/110/187/012/main.jpg
+住所：新潟県新潟市秋葉区朝日７０－１
+営業時間：10:00～19:00 
+定休日：毎週火曜日定休　年末クリスマス休暇、年始は元旦から営業
+<div id="mapbox">
+  <noscript>
+    このページは JavaScriptを使用しているため、正しく表示が出来ません。<br>申し訳ありませんが、JavaScript対応ブラウザでご利用ください。
+  </noscript>
+</div>
+=end
+#------------------------------------------------------------------
+=begin
+  def self.get_store(page, car)
+    link = page.at('body > div.page > div:nth-child(8) > div > div.column__sub > div.sideBoxWrap--primary > div.sideBoxWrap__contents > div.shopInfo > h3 > a')
+    store_page = agent.get(link[:href])
+    return unless store_page #store_pageが存在していない場合、処理しない
+    #-------------------------------------
+    #　　　　　　　　　　店名
+    #-------------------------------------
+    store_name = store_page.at('/html/body/div[1]/div[3]/aside/div/div[2]/section/h2')
+    #-------------------------------------
+    #　　　　　　　　　　写真
+    #-------------------------------------
+    store_image = store_page.at('/html/body/div[1]/div[3]/aside/div/div[1]/a/img')
+    store_image_link = store_image[:src]
+    store_image_link[0..1] = ""
+    #-------------------------------------
+    #　　　　　　　　　　住所
+    #-------------------------------------
+    store_adress = store_page.at('/html/body/div[1]/div[3]/aside/div/div[2]/section/dl/dd[2]').inner_text
+    length = store_adress.length
+    store_adress[length-3..length-1] = ''
+    #-------------------------------------
+    #　　　　　　　　　営業時間
+    #-------------------------------------
+    store_business_hours = store_page.at('/html/body/div[1]/div[3]/aside/div/div[2]/section/dl/dd[3]').inner_text
+    #-------------------------------------
+    #　　　　　　　　　定休日
+    #-------------------------------------
+    store_regular_holiday = store_page.at('/html/body/div[1]/div[3]/aside/div/div[2]/section/dl/dd[4]').inner_text
+    #-------------------------------------
+    #　　　　　　　　　　地図
+    #-------------------------------------
+    #地図情報が取っているんだが、使えないため、住所からgoogle map apiを使って生成すること
+    store_access_map_link = store_page.at('/html/body/div[1]/div[4]/div/div/p[2]/a')[:href]
+    return unless store_page#mapのリンクが存在していない場合、処理しない
+    store_access_map_page = agent.get(store_access_map_link)
+    store_access_map = store_access_map_page.at('#mapbox')
+    #----------------------------------------------------------
+    #　　　　　　　　　　   データベースに保存
+    #----------------------------------------------------------
+    store = car.condition || car.build_condition
+    store.name            = store_name
+    store.image_link      = store_image_link
+    store.adress          = store_adress
+    store.business_hours  = store_business_hours
+    store.regular_holiday = store_regular_holiday
+    store.access_map_link = store_access_map
+    store.save!
+  end
+
+=end
