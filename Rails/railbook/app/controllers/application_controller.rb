da91ba8cb080cff3dede197010429dd475bac438
SuperLazyDog <jyoitatsu@foxmail.com>
2017-11-06 23:50:41 +0900

    增加服务器端params的输出

--- Rails/railbook/app/controllers/application_controller.rb
+++ Rails/railbook/app/controllers/application_controller.rb
@@ -4,8 +4,8 @@ class ApplicationController < ActionController::Base
   before_action :detect_device
   add_flash_types :test_flash
   add_flash_types :info
+  after_action :debug_mode_in_server
 
-  
   private
     def detect_device
       case params[:type]
@@ -15,5 +15,26 @@ class ApplicationController < ActionController::Base
           render plain: 'tablet'
       end
     end
+
+  def debug_mode_in_server
+    case ENV['RAILS_ENV']
+    when "development", "test"
+        puts %q(-------------------------------------------------------------------)
+        puts %q(                        debugger mode)
+        puts %q(-------------------------------------------------------------------)
+        puts %Q(environment: #{ENV['RAILS_ENV']})
+        puts %q(------------------------------------------)
+        puts %q(                params)
+        puts %q(------------------------------------------)
+        if params.present?
+          pp params
+          ap params#.permit(:action, :controller)
+        end
+        puts %Q(\n\n)
+        # puts %q(                       finish debugger)
+        # puts %q(-------------------------------------------------------------------)
+      when "production"
+    end
+  end
 
 end
