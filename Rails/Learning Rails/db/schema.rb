811dc7e757b55ed32668e7061211464b6e436cc7
SuperLazyDog <jyoitatsu@foxmail.com>
2018-03-31 16:42:49 +0900

    Done-step: [增加子文件夹的model]

--- Rails/Learning Rails/db/schema.rb
+++ Rails/Learning Rails/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170626185533) do
+ActiveRecord::Schema.define(version: 20180331073514) do
 
   create_table "authors", force: :cascade do |t|
     t.integer "user_id"
@@ -92,6 +92,12 @@ ActiveRecord::Schema.define(version: 20170626185533) do
     t.datetime "updated_at", null: false
     t.string "testStr", default: "abcdefg"
     t.string "testInModeltest2_File"
+  end
+
+  create_table "pro_sample_models", force: :cascade do |t|
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "questionnaires", force: :cascade do |t|
