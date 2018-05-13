7a5201edfcde68fcbaef111a30fd7f6a74f546d8
SuperLazyDog <jyoitatsu@foxmail.com>
2018-05-13 17:39:19 +0900

    Temp: [S003]

--- programming_test/paiza/S003/S003.rb
+++ programming_test/paiza/S003/S003.rb
@@ -1,7 +1,29 @@
 class DataOperater
   attr_accessor :data, :result
+  #----------------------------------------------------------------
+  #                    データの初期化と入力取得
+  #----------------------------------------------------------------
   def initialize
-    @datas = []
+    # datas
+    # {all_people_count: num, consults: []}
+    # {
+    #   info: {
+    #     all_people_count: 100,
+    #     cousults_count: n,
+    #   }
+    #   consults: [
+    #     "1 said 3 was an honest person.",
+    #     "2 said 3 was a liar.",
+    #     ...
+    #   ]
+    # }
+    @datas = {
+      info: {
+        all_people_count:0,
+        cousults_count: 0
+        },
+      consults:[]
+      }
     @result = []
   end
 
@@ -15,160 +37,114 @@ class DataOperater
     File.open('sample_data.txt') do |file|
       case_temp = []
       is_new_case = true
-      file.each_line do |line|
-        @datas.push line.chomp
+      file.each_line.with_index(1) do |line, i|
+        data_line = line.chomp
+        if i == 1
+          datas = data_line.split(' ')
+          @datas[:info][:all_people_count] = datas[0].to_i
+          @datas[:info][:cousults_count] = datas[1].to_i
+        else
+          @datas[:consults].push data_line
+        end
       end
     end
-    self.data_initialize_debug # TODO: delete here
+    self.data_build_debug # TODO: delete here
 
     # stdinによるデータ初期化
     # @datas.push $stdin.gets
-    # self.data_initialize_debug # TODO: delete here
+    # self.data_build_debug # TODO: delete here
   end
+
   #　データの初期化のデバッグ
-  def data_initialize_debug
+  def data_build_debug
     puts "@datas: #{@datas}"
   end
-
+  #----------------------------------------------------------------
+  #                         処理ロジック
+  #----------------------------------------------------------------
   #　出力のための処理
   def processer
-    letters = ('a'..'z').to_a
-    numbers = (0..9).to_a
-    numbers_strs = ('0'..'9').to_a
-    brackets = ['(', ')']
-    is_letter = Proc.new { |char| letters.include? char }
-    is_number = Proc.new { |char| numbers_strs.include? char }
-    is_bracket = Proc.new { |char| brackets.include? char }
-    get_flag = Proc.new { |char|
-      next 0 if is_letter.call char
-      next 1 if is_number.call char
-      next 2 if char == brackets[0]
-      next 3 if char == brackets[1]
-    }
-    result_temp = {}
-    reset_result_temp = Proc.new {
-      letters.each do |letter|
-        result_temp[letter] = 0
-      end
-    }
-    build_result = Proc.new {
-      result_temp.each do |k, v|
-        @result.push format_output(k, v)
-      end
-    }
-    @datas.each do |data|
-      operater_temp = []
-      queue = data.split ''
-      number_str = ''
-      reset_number_str = Proc.new {
-        number_str = ''
-      }
-      reset_result_temp.call
-      # 文字列から演算単位の配列へ変換
-      while !queue.empty?
-        char = queue.shift
-        case get_flag.call char # 0: letter, 1: number, 2: ( 3: )
-        when 0
-          operater_temp.push char
-        when 1
-          number_str << char
-          while get_flag.call(queue.first) == 1
-            char = queue.shift
-            number_str << char
-          end
-          operater_temp.push number_str
-          reset_number_str.call
-        when 2
-          operater_temp.push char
-        when 3
-          operater_temp.push char
-        end
-      end
-      # puts "operater_temp: #{operater_temp}"
-
-      # 演算単位の配列から計算
-      count_letters operater_temp, result_temp
-    end
-    build_result.call
+    result_map = Array.new(@datas[:info][:all_people_count]+1) # 0..<@datas[:info][:all_people_count]
+    queue = self.build_process_queue
+    result = 0
+    puts "queue: #{queue}"
+    count = recursive(queue, result_map, true)
+    puts "recursive: #{count}"
   end
 
-  # 演算単位の配列から計算
-  def count_letters(operater_temp, result_temp)
-    puts "operater_temp: #{operater_temp}"
-    letters = ('a'..'z').to_a
-    numbers = (0..9).to_a
-    numbers_strs = ('0'..'9').to_a
-    brackets = ['(', ')']
-    is_letter = Proc.new { |char| letters.include? char }
-    is_bracket = Proc.new { |char| brackets.include? char }
-    get_flag = Proc.new { |char|
-      next 0 if is_letter.call char
-      next 2 if char == brackets[0]
-      next 3 if char == brackets[1]
-      next 1
-    }
-    operaters = {}
-    coefficient = 1
-    while true
-      break if operater_temp.empty?
-      item = operater_temp.shift
-      flag =  get_flag.call item  # 0: letter, 1: number, 2: ( 3: )
-      if flag == 2 # (...)の処理
-        level = 1
-        number_queue = []
-        level_index = []
-        while level > 0 # 絶対キューの範囲内なので、emptyチェックなしで
-          item = operater_temp.shift
-          flag =  get_flag.call item
-          case flag
-          when 0 # letter
-            puts "level_index[#{level}]: #{level_index[level]}"
-            result_temp[item] += coefficient
-            coefficient = coefficient/level_index[level] unless level_index[level].nil?
-            level_index.delete_at level
-          when 1 # number
-            item_num = item.to_i
-            level_index[level] = item_num
-            number_queue.push item_num
-            coefficient *= item_num
-          when 2 # (
-            level += 1
-            # 12 (
-          when 3 # )
-            coefficient /= level_index[level-1] unless level_index[level-1].nil?
-            level -= 1
-            level_index.delete_at level
-          end
-          puts "--------------------------------------"
-          puts "level: #{level}"
-          puts "item: #{item}"
-          puts "result_temp[#{item}]: #{result_temp[item]}" unless result_temp[item].nil?
-          puts "level_index: #{level_index}"
-          # puts "number_queue: #{number_queue}"
-          puts "coefficient: #{coefficient}"
-        end
-        coefficient = 1
-      elsif flag == 0
-        result_temp[item] += coefficient
-        coefficient = 1
-      elsif flag == 1
-        coefficient *= item.to_i
-      end
-      # puts "item: #{item}, coefficient: #{coefficient}"
-    end
-    # while !operater_temp.empty?
-    #   item = operater_temp.shift
-    #   flag =  get_flag.call item  # 0: letter, 1: number, 2: ( 3: )
-    #   if flag == 2
-    #   else
-    #   end
-    # end
-    puts "result_temp: #{result_temp}"
+  def build_process_queue
+    @datas[:consults]
   end
 
-  def format_output(alpha, count)
-    #Case #1: 2 44.72135955
-    str = "#{alpha} #{count}\n"
+  def recursive(queue, result_map, consume_situation)
+    puts "---------------------------------------"
+    puts "result_map: #{result_map.drop(1)}"
+    puts "queue: #{queue}"
+    countA = 0
+    countB = 0
+    unless queue.empty?
+      current_data = queue.shift
+      # A
+      result_mapA = result_map
+      item = analyze_data current_data, consume_situation
+      speaker_index = item[:speaker][:index];speaker_is_honest = item[:speaker][:result]
+      target_index = item[:target][:index];target_is_honest = item[:target][:result]
+      result_mapA[speaker_index] = speaker_is_honest if result_mapA[speaker_index].nil?
+      result_mapA[target_index] = target_is_honest if result_mapA[target_index].nil?
+      if result_mapA[speaker_index] != speaker_is_honest || result_mapA[target_index] != target_is_honest
+        countA = 0
+      else
+        countA += recursive queue, result_mapA, consume_situation
+      end
+
+
+
+      # b
+      result_mapB = result_map
+      item = analyze_data current_data, !consume_situation
+      speaker_index = item[:speaker][:index];speaker_is_honest = item[:speaker][:result]
+      target_index = item[:target][:index];target_is_honest = item[:target][:result]
+      result_mapB[speaker_index] = speaker_is_honest if result_mapB[speaker_index].nil?
+      result_mapB[target_index] = target_is_honest if result_mapB[target_index].nil?
+      if result_mapB[speaker_index] != speaker_is_honest || result_mapB[target_index] != target_is_honest
+        countB = 0
+      else
+        countB += recursive queue, result_mapA, !consume_situation
+      end
+    else
+      puts "OKOK"
+      count = 0
+      puts "result_map: #{result_map.drop(1)}"
+      result_map.drop(1).each do |result|
+        if result == nil
+          count += 1
+        end
+      end
+      return 2**count
+    end
+  end
+
+  def analyze_data(str, consume_speaker_honest=true)
+    # => {speak: {index:, result:}, target: {index:, result:}}
+    elements = str.split(' ')
+    speaker = elements[0].to_i
+    target = elements[2].to_i
+    express_thread = elements[4]
+    {
+      speaker: { index: speaker, result: consume_speaker_honest },
+      target: { index: target, result: express_thread.size == 2 }
+    }
+  end
+
+  #----------------------------------------------------------------
+  #                         データの出力
+  #----------------------------------------------------------------
+  def build_result(data)
+    # ここに出力のためのデータ@resultを　 に作る
+  end
+  def format_output_str(count)
+    # count
+    str = "count\n"
   end
 
   def show_result
@@ -176,6 +152,9 @@ class DataOperater
   end
 end
 
+#----------------------------------------------------------------
+#                        　 実行
+#----------------------------------------------------------------
 dataOperater = DataOperater.new
 dataOperater.initialize_data
 dataOperater.processer
