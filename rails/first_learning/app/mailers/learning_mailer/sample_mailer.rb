93ccfe91fd614b8c469c30dd094f06be00d6c4b7
SuperLazyDog <jyoitatsu@foxmail.com>
2019-10-01 11:39:39 +0900

    Temp: [生成测试用mailer]

---
+++ rails/first_learning/app/mailers/learning_mailer/sample_mailer.rb
@@ -0,0 +1,46 @@
+class LearningMailer::SampleMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.learning_mailer.sample_mailer.t1.subject
+  #
+  def t1
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.learning_mailer.sample_mailer.t2.subject
+  #
+  def t2
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.learning_mailer.sample_mailer.t3.subject
+  #
+  def t3
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.learning_mailer.sample_mailer.t4.subject
+  #
+  def t4
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+end
