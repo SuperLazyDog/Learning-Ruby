283a690d07629638a10f3d536365a8799cb51d0e
SuperLazyDog <weida.xu@super-studio.jp>
2024-04-05 19:38:01 +0900

    setup sidekiq for unicorn

--- plugin/unicorn/config/environments/production.rb
+++ plugin/unicorn/config/environments/production.rb
@@ -59,6 +59,7 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "unicorn_production"
+  # config.active_job.queue_adapter = :sidekiq # UUID Weida: Use Sidekiq as Active Job queue adapter
 
   config.action_mailer.perform_caching = false
 
--- plugin/unicorn/config/routes.rb
+++ plugin/unicorn/config/routes.rb
@@ -1,4 +1,5 @@
+require 'sidekiq/web'
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  mount Blorgh::Engine, at: "/blog"
+  mount Blorgh::Engine, at: "/"
 end
