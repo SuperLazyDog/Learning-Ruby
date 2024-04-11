67c6a5227988512f3c6349b28654b861ed4b0975
SuperLazyDog <weida.xu@super-studio.jp>
2024-04-11 17:30:49 +0900

    add model User into blorgh/test/dummy

--- plugin/blorgh/test/dummy/db/schema.rb
+++ plugin/blorgh/test/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_03_25_103801) do
+ActiveRecord::Schema.define(version: 2024_04_11_082832) do
 
   create_table "blorgh_articles", force: :cascade do |t|
     t.string "title"
@@ -23,6 +23,12 @@ ActiveRecord::Schema.define(version: 2024_03_25_103801) do
   create_table "blorgh_comments", force: :cascade do |t|
     t.integer "article_id"
     t.text "text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
